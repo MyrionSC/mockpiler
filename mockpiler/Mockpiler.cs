1e8fc4cae50f06260f317d3981d695cd1c866e09
Martin RA <marpht23@gmail.com>
2021-04-02 21:27:11 +0200

    clean up test classes

--- mockpiler/Mockpiler.cs
+++ mockpiler/Mockpiler.cs
@@ -10,38 +10,10 @@ namespace business_layer
     {
         public static string StartMockpile(string jsonString)
         {
-            Outer outer = new() {
-                Nested1 = new() {
-                    SomeString = "somestring",
-                    SomeInt = 42,
-                    SomeDouble = 42.5,
-                    SomeDateTime = DateTime.Parse("2021-04-02T09:00:34"),
-                    SomeStringList = new List<string> {
-                        "item1", "item2"
-                    },
-                    SomeClassList = new List<InnerClass> {
-                        new () {
-                            InnerClassInt = 2,
-                            InnerClassString = "lala"
-                        },
-                        new() {
-                            InnerClassInt = 3,
-                            InnerClassString = "lolo"
-                        }
-                    }
-                },
-                Nested2 = new() {
-                    SomeString = "somestring",
-                    SomeInt = 42,
-                    SomeDouble = 42.5,
-                    SomeDateTime = DateTime.Parse("2021-04-02T09:00:34")
-                }
-            };
-
             Dictionary<string, object> dict = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
             return Mockpile(dict);
         }
-        
+
         public static string Mockpile(object input)
         {
             return input switch {
@@ -56,7 +28,7 @@ namespace business_layer
                 _ => throw new Exception($"Mockpiler hit default with object of type {input.GetType()}")
             };
         }
-        
+
         public static string Mockpile(Dictionary<string, object> input)
         {
             List<string> resultList = input.Select(pair => $"{pair.Key} = {Mockpile(pair.Value)}").ToList();
@@ -68,19 +40,19 @@ namespace business_layer
         {
             return Mockpile(input.ToObject<Dictionary<string, object>>());
         }
-        
+
         public static string Mockpile(JValue input)
         {
             return Mockpile(input.ToObject<object>());
         }
-        
+
         // TODO: Try to guess array type from first element
         public static string Mockpile(JArray input)
         {
             List<string> resultList = input.Select(Mockpile).ToList();
-            return $"new List<dynamic> {{\n{string.Join(",\n", resultList)}\n}}" ;
+            return $"new List<dynamic> {{\n{string.Join(",\n", resultList)}\n}}";
         }
-        
+
         public static string Mockpile(double input)
         {
             return input.ToString(System.Globalization.CultureInfo.InvariantCulture); // commas as dots
@@ -88,36 +60,12 @@ namespace business_layer
 
         public static string Mockpile(DateTime input)
         {
-            return $"DateTime.Parse(\"{ input.ToString("s", System.Globalization.CultureInfo.InvariantCulture) }\")";
+            return $"DateTime.Parse(\"{input.ToString("s", System.Globalization.CultureInfo.InvariantCulture)}\")";
         }
 
         public static string Mockpile(string input)
         {
             return $"\"{input}\"";
         }
-
-    }
-
-    public class SomeClass
-    {
-        public string SomeString;
-        public int SomeInt;
-        public double SomeDouble;
-        public DateTime SomeDateTime;
-        public List<string> SomeStringList { get; set; }
-        // public List<int> SomeIntList { get; set; }
-        public List<InnerClass> SomeClassList { get; set; }
-    }
-
-    public class InnerClass
-    {
-        public int InnerClassInt;
-        public string InnerClassString;
-    }
-
-    public class Outer
-    {
-        public SomeClass Nested1;
-        public SomeClass Nested2;
     }
 }
