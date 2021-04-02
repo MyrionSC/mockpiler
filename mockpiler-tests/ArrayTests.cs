92fd7028ea564fc0f8c0d4f57488a43d2b678525
Martin RA <marpht23@gmail.com>
2021-04-02 10:11:24 +0200

    wrong test file names

--- mockpiler-tests/ArrayTests.cs
+++ mockpiler-tests/ArrayTests.cs
@@ -1,67 +1,13 @@
 using System;
 using business_layer;
-using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace business_layer_test
 {
-    public class ArrayTests
+    public class SimpleTypeTests
     {
         [Test]
-        public void EmptyExactMatch()
-        {
-            string input = @"{}";
-            string actual = Mockpiler.ExecuteMockpile(input);
-            string expected = "{}";
-            TestHelper.AssertEqualNoWhitepace(expected, actual);
-        }
-        
-        [Test]
-        public void InvalidJson()
-        {
-            string input = @"{";
-            Assert.Throws<JsonSerializationException>(() => Mockpiler.ExecuteMockpile(input));
-        }
-
-
-        [Test]
-        public void SingleStringExactMatch()
-        {
-            string input = @"{""La"": ""lo""}";
-            string actual = Mockpiler.ExecuteMockpile(input);
-            string expected = @"{ La = ""lo"" }";
-            TestHelper.AssertEqualNoWhitepace(expected, actual);
-        }
-
-        [Test]
-        public void SingleIntExactMatch()
-        {
-            string input = @"{}";
-            string actual = Mockpiler.ExecuteMockpile(input);
-            string expected = "{}";
-            TestHelper.AssertEqualNoWhitepace(expected, actual);
-        }
-
-        [Test]
-        public void SingleDoubleExactMatch()
-        {
-            string input = @"{}";
-            string actual = Mockpiler.ExecuteMockpile(input);
-            string expected = "{}";
-            TestHelper.AssertEqualNoWhitepace(expected, actual);
-        }
-
-        [Test]
-        public void SingleDateExactMatch()
-        {
-            string input = @"{}";
-            string actual = Mockpiler.ExecuteMockpile(input);
-            string expected = "{}";
-            TestHelper.AssertEqualNoWhitepace(expected, actual);
-        }
-
-        [Test]
-        public void SingleDateTimeExactMatch()
+        public void SingleArrayEmptyExactMatch()
         {
             string input = @"{}";
             string actual = Mockpiler.ExecuteMockpile(input);
--- mockpiler-tests/SimpleTypeTests.cs
+++ mockpiler-tests/SimpleTypeTests.cs
@@ -1,13 +1,66 @@
 using System;
 using business_layer;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace business_layer_test
 {
-    public class SimpleTypeTests
+    public class ArrayTests
     {
         [Test]
-        public void SingleArrayEmptyExactMatch()
+        public void EmptyExactMatch()
+        {
+            string input = @"{}";
+            string actual = Mockpiler.ExecuteMockpile(input);
+            string expected = "{}";
+            TestHelper.AssertEqualNoWhitepace(expected, actual);
+        }
+        
+        [Test]
+        public void InvalidJson()
+        {
+            string input = @"{";
+            Assert.Throws<JsonSerializationException>(() => Mockpiler.ExecuteMockpile(input));
+        }
+
+        [Test]
+        public void SingleStringExactMatch()
+        {
+            string input = @"{""La"": ""lo""}";
+            string actual = Mockpiler.ExecuteMockpile(input);
+            string expected = @"{ La = ""lo"" }";
+            TestHelper.AssertEqualNoWhitepace(expected, actual);
+        }
+
+        [Test]
+        public void SingleIntExactMatch()
+        {
+            string input = @"{}";
+            string actual = Mockpiler.ExecuteMockpile(input);
+            string expected = "{}";
+            TestHelper.AssertEqualNoWhitepace(expected, actual);
+        }
+
+        [Test]
+        public void SingleDoubleExactMatch()
+        {
+            string input = @"{}";
+            string actual = Mockpiler.ExecuteMockpile(input);
+            string expected = "{}";
+            TestHelper.AssertEqualNoWhitepace(expected, actual);
+        }
+
+        [Test]
+        public void SingleDateExactMatch()
+        {
+            string input = @"{}";
+            string actual = Mockpiler.ExecuteMockpile(input);
+            string expected = "{}";
+            TestHelper.AssertEqualNoWhitepace(expected, actual);
+        }
+
+        [Test]
+        public void SingleDateTimeExactMatch()
         {
             string input = @"{}";
             string actual = Mockpiler.ExecuteMockpile(input);
